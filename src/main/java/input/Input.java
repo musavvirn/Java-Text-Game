01b87d6cd709222b6b0c0274e921b6769ec6b63e
musavvir.noor <musavvir.noor@gmail.com>
2020-06-26 18:16:37 -0700

    extracted userinput into a separate Singleton

---
+++ src/main/java/input/Input.java
@@ -0,0 +1,9 @@
+package input;
+
+public enum Input {
+    /* B = Go back for choices */
+    B,
+
+    /* I = Go to Mission Log */
+    M
+}
---
+++ src/main/java/input/UserInput.java
@@ -0,0 +1,55 @@
+package input;
+
+import choice.Choice;
+import mission.MissionManager;
+
+import java.util.Scanner;
+
+/* Obtains user input
+    SINGLETON
+ */
+
+public class UserInput {
+    private static UserInput instance = new UserInput();
+
+    public static UserInput getInstance() {
+        return instance;
+    }
+
+    private UserInput() {}
+
+    /* Determine user input, either a valid choice, (B), or invalid and promp again */
+
+    public Choice getUserSelection(Choice choice) throws Exception {
+        String input = new Scanner(System.in).next();
+        int num = 100;
+
+        // Detect "Go Back" option selected with key - "b" or "B"
+        if (input.equalsIgnoreCase(Input.B.toString())) {
+            choice.getParentChoice().runChoiceSelection();
+
+        } else if (input.equalsIgnoreCase(Input.M.toString())) {
+            MissionManager.getInstance().printMissions();
+            // transition from Mission Log to Choices again
+
+        } else {
+            try {
+                num = Integer.parseInt(input);
+            } catch (Exception e) {
+                System.out.println(String.format("Invalid. Choose one from: "));
+                choice.runChoiceSelection();
+            }
+        }
+
+        Choice c = null;
+        try {
+            c = choice.getChoices().get(num);
+            System.out.println(String.format("%d - selected.", num));
+            c.printActiveText();
+        } catch (Exception e) {
+            System.out.println(String.format("Invalid. Choose one from: "));
+            choice.runChoiceSelection();
+        }
+        return c;
+    }
+}
