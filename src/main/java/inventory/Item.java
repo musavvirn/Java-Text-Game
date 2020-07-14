51ba69b26ae4446cf854a5447aab19d92ff22ebe
musavvir.noor <musavvir.noor@gmail.com>
2020-07-13 20:58:47 -0700

    updated Items

--- src/main/java/inventory/Item.java
+++ src/main/java/inventory/Item.java
@@ -1,14 +1,49 @@
 package inventory;
 
 public class Item {
-    private String name;
+    protected String name;
+    protected int price;
+    protected ItemState state;
 
-    public Item(String name) {
+    public Item(String name, int price) {
         this.name = name;
+        this.state = ItemState.TRADEABLE;
+        this.price = price;
+    }
+
+    public Item(String name, ItemState state) {
+        this.name = name;
+        this.state = state;
+        this.price = 0;
     }
 
     public String getName() {
         return this.name;
     }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public ItemState getState() {
+        return state;
+    }
+
+    public void setState(ItemState state) {
+        this.state = state;
+    }
+
+    public Item(String name) {
+        this.name = name;
+        this.state = ItemState.TRADEABLE;
+        this.price = 0;
+    }
+
+
+
 
 }
---
+++ src/main/java/inventory/ItemState.java
@@ -0,0 +1,5 @@
+package inventory;
+
+public enum ItemState {
+    TRADEABLE, UNTRADEABLE
+}
